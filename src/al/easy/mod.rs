e34f16e6fc97bc64f9210a9e02ce9eb8e5153669
zonghenghhuanyu <ywpdms@outlook.com>
2022-12-13 18:14:29 +0800

    confilt

--- src/al/easy/mod.rs
+++ src/al/easy/mod.rs
@@ -2,6 +2,8 @@ pub mod n_104_deep_of_tree;
 pub mod n_1470;
 pub mod n_1800;
 pub mod n_94;
+pub mod n_26;
+
 
 //遍历二维数组
 #[cfg(test)]
---
+++ src/al/easy/n_26.rs
@@ -0,0 +1,37 @@
+
+#[cfg(test)]
+mod tttt{
+
+    #[test]
+    fn works(){
+        let mut nums = vec![1,2,2,2,3,3,4,5,6,7,8];
+        let ans = remove_duplicates(&mut nums);
+        println!("{}:{:?}",ans,nums)
+    }
+
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+
+        let len = nums.len();
+        if len == 1{
+            return 1;
+        }
+
+        let mut pri = 0;
+        let mut tail = 1;
+
+        loop {
+            
+            if nums[pri] == nums[tail]{
+                tail += 1;
+            }else {
+                pri += 1;
+                nums[pri] = nums[tail];
+                tail += 1;
+            }
+            if tail == len{
+                break;
+            }
+        }
+        (pri + 1) as i32
+    }
+}
